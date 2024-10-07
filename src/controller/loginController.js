44fe3423f8b59e0076a40e79a4beaa2a620f4251
Rayane Macedo Carvalho <ra49413593809@cursos.insf>
2024-10-07 18:21:28 -0300

    vapo-footer

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -5,21 +5,19 @@ import consultarAdminService from "../service/loginService.js";
 const endpoints = Router();
 
 
-endpoints.post('/login/adm', async (req,resp) => {
+endpoints.post('/login/adm', async (req, resp) => {
     try {
-        let usuario = req.query.usuario;
-        let senha = req.query.senha;
+        let usuario = req.body.usuario;
+        let senha = req.body.senha; 
 
         let token = await consultarAdminService(usuario, senha);
 
-        resp.send({token});
+        resp.send({ token });
+    } catch (err) { 
+        console.error('Login error:', err); 
+        resp.status(401).send({ message: 'Credenciais inválidas.' }); 
     }
-    catch (err) { 
-        logErro(err);
-        resp.status(401).send(criarErro(err));
-    }
-})
-
+});
 
 
 export default endpoints;
