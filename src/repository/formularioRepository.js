c35d30851afa87c0e96eee425ed70d40c31bff13
jucastro15 <ra56421610838@acaonsfatima.org.br>
2024-10-19 21:45:28 -0300

    form

--- src/repository/formularioRepository.js
+++ src/repository/formularioRepository.js
@@ -1,37 +1,34 @@
 import con from "./connection.js";
 
-
 export async function candidatoFormulario(candidato) {
     const comando = `
-        INSERT INTO formularios (nome,cpf, cargo, email, curriculo, data_inscricao)
-        VALUES (?, ?, ?, sysdate(), id_vagas)
+        INSERT INTO formularios (nome, cpf, id_vaga, email, curriculo, data_inscricao)
+        VALUES (?, ?, ?, ?, ?, NOW())
     `;
 
-    let resposta = await con.query(comando, [candidato.nome, candidato.cpf, candidato.cargo, candidato.email, candidato.curriculo]);
+    let resposta = await con.query(comando, [
+        candidato.nome,
+        candidato.cpf,
+        candidato.id_vaga, 
+        candidato.email,
+        candidato.curriculo
+    ]);
 
     let info = resposta[0];
-
-
     return info.insertId;
 }
 
-
-
 export async function consultarCandidatos() {
-  const comando = `select*from formularios`
-  let [resultado] = await con.query(comando)
-  return resultado
+    const comando = `SELECT * FROM formularios`;
+    let [resultado] = await con.query(comando);
+    return resultado;
 }
-
 
 export async function consultarCandidatosPorID(id) {
-  const comando = `
-  select*from formularios
-  where id = ?`
-
-  let [resultado] = await con.query(comando, [id])
-  let info = resultado[0]
-  return info
- 
+    const comando = `
+        SELECT * FROM formularios
+        WHERE id = ?
+    `;
+    let [resultado] = await con.query(comando, [id]);
+    return resultado[0];
 }
-  
