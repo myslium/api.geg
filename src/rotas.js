695c8189ecf5702ef4bf643d75f80be9a1a226fb
jucastro15 <ra56421610838@acaonsfatima.org.br>
2024-10-26 10:17:46 -0300

    opa

--- src/rotas.js
+++ src/rotas.js
@@ -1,28 +1,25 @@
-import candidatofinalController from  '../src/controller/candidatofinalController.js';
-import vagasController from  '../src/controller/vagasController.js'
-
+import candidatofinalController from '../src/controller/candidatofinalController.js';
+import vagasController from '../src/controller/vagasController.js'
 import emailController from '../src/controller/emailController.js'
-import receitaController  from  '../src/controller/receitaController.js'
-import loginController from   '../src/controller/loginController.js'
+import receitaController from '../src/controller/receitaController.js'
+import loginController from '../src/controller/loginController.js'
 import interesseController from "../src/controller/interesseController.js";
 import formularioController from '../src/controller/formularioController.js'
 import conteudoController from '../src/controller/conteudoController.js'
 
 
-export default function Rotas(servidor){
+export default function Rotas(servidor) {
 
 
-  servidor.use(emailController)
+   servidor.use(emailController)
    servidor.use(loginController)
-
-servidor.use( receitaController )
+   servidor.use(receitaController)
    servidor.use(interesseController)
- 
-    servidor.use(loginController)
-    servidor.use(candidatofinalController);
-    servidor.use(vagasController);
-    servidor.use(formularioController);
-    servidor.use(conteudoController);
+   servidor.use(loginController)
+   servidor.use(candidatofinalController);
+   servidor.use(vagasController);
+   servidor.use(formularioController);
+   servidor.use(conteudoController);
 
 
 }       
