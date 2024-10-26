a8d3563e361b50e8ff0fe92f6e2d995587a38db7
Mariana <165081625+myslium@users.noreply.github.com>
2024-10-26 13:21:13 -0300

    .

--- src/rotas.js
+++ src/rotas.js
@@ -1,29 +1,17 @@
 import candidatofinalController from  '../src/controller/candidatofinalController.js';
 import vagasController from  '../src/controller/vagasController.js'
-<<<<<<< HEAD
-
+import receitaController from '../src/controller/receitaController.js'
 import emailController from '../src/controller/emailController.js'
 import loginController from  '../src/controller/loginController.js'
-=======
-import receitaController  from  '../src/controller/receitaController.js'
-import loginController from   '../src/controller/loginController.js'
-import interesseController from "../src/controller/interesseController.js";
->>>>>>> 43a2d09d906c319860080c1fd75e0ec370113db1
 import formularioController from '../src/controller/formularioController.js'
 import conteudoController from '../src/controller/conteudoController.js'
+import interesseController from '../src/controller/interesseController.js'
 
 
 export default function Rotas(servidor){
-
-<<<<<<< HEAD
-  servidor.use(emailController)
-   servidor.use(loginController)
-=======
-
-servidor.use( receitaController )
+   servidor.use(emailController)
+    servidor.use( receitaController )
    servidor.use(interesseController)
- 
->>>>>>> 43a2d09d906c319860080c1fd75e0ec370113db1
     servidor.use(loginController)
     servidor.use(candidatofinalController);
     servidor.use(vagasController);
