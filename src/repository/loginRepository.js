ef4f759f142fc5ef6e647b3c72a788cb0940e0f9
jucastro15 <ra56421610838@acaonsfatima.org.br>
2024-11-03 00:22:56 -0300

    vaiiii

--- src/repository/loginRepository.js
+++ src/repository/loginRepository.js
@@ -1,16 +1,21 @@
-import con from './connection.js'
+import CryptoJS from 'crypto-js';
+import con from './connection.js';
+
 
 export default async function consultarAdmin(usuario, senha) {
+  
+    const senhaCriptografada = CryptoJS.SHA256(senha).toString(); 
+
     let comando = `
-          SELECT DS_USUARIO   usuario,
-                 DS_SENHA     senha
+          SELECT DS_USUARIO usuario,
+                 DS_SENHA   senha
             FROM TB_ADMIN
            WHERE DS_USUARIO = ? 
              AND DS_SENHA   = ?;
-    `
+    `;
 
-    let resposta = await con.query(comando, [usuario, senha]);
+    let resposta = await con.query(comando, [usuario, senhaCriptografada]); 
     let linhas = resposta[0];
-    return linhas[0];
-}
 
+    return linhas[0]; 
+}
